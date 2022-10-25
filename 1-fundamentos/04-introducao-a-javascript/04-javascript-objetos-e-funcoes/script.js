e4db5559b4c636b4e7fc3b14cb013989beb2bab2
Rafael Oliveira <fael.odn@gmail.com>
2022-10-25 16:16:33 -0300

    Exercicio de Objeto

---
+++ 1-fundamentos/04-introducao-a-javascript/04-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,21 @@
+let player = {
+    name:'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {golden: 2, silver: 3},
+}
+console.log ('A jogadora ' +player['name'] + ' ' + player['lastName'] + ' tem ' + player.age + ' anos de idade');
+console.log ('A jogador possui ' + player.medals.golden + ' medalhas de ouro, e ' + player.medals.silver + ' de prata.')
+
+
+
+
+// Para fixar
+// Vamos fazer alguns exercícios de fixação para consolidar os conhecimentos adquiridos! 
+
+// Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+// 
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
